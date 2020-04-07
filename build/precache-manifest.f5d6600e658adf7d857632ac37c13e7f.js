d8cf3b6c477b34805cfc0dd26faad7f5931ba86f
Jack-Andrew-Robert-Sanderson <jacksanderson123@hotmail.co.uk>
2020-04-07 18:33:32 +0100

    setup project

---
+++ build/precache-manifest.f5d6600e658adf7d857632ac37c13e7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2e362298dd67075e26",
+    "url": "/static/css/main.ec6c776c.chunk.css"
+  },
+  {
+    "revision": "ee2e362298dd67075e26",
+    "url": "/static/js/main.ee2e3622.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "a7e6419866ddb85636869c5ee7b23c98",
+    "url": "/index.html"
+  }
+];
